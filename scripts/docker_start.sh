630729c643b690c3adf7700eb266cb91b6f96a3b
Pankaj Gupta <pankaj.gupta@sworks.co.in>
2022-09-08 03:59:00 +0530

    pipeline

--- scripts/docker_start.sh
+++ scripts/docker_start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker run -d -p 3099:9000 --name attendance-management-dev attendance-management:latest
 docker logs attendance-management-dev
-#rm -rf /var/www/html/dev-attendance/*
-ls -lhtr
+rm -rf /var/www/html/dev-attendance/*
