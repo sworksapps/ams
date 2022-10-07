d93f33da5fb88b26091a7944f2a0899b7646a56e
pankaj.gupta@sworks.co.in <pankaj.gupta@sworks.co.in>
2022-10-07 11:32:51 +0000

    Edited docker_start.sh

--- scripts/docker_start.sh
+++ scripts/docker_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker run -d -p 3089:9000 --name attendance-management-$ATTENDANCE_MANAGEMENT_ENV attendance-management:latest
-docker logs attendance-management-$ATTENDANCE_MANAGEMENT_ENV  
+docker run -d -p 3089:9000 --name attendance-management-uat attendance-management-uat:latest
+docker logs attendance-management-uat  
 rm -rf /var/www/html/attendance
 mkdir -p  /var/www/html/attendance
 
