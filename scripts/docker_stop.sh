98ffadcf507907e2deb8e6fa1e854ea858e113ad
49175 <pankaj.gupta@sworks.co.in>
2022-10-07 11:34:04 +0000

    Edited docker_stop.sh

--- scripts/docker_stop.sh
+++ scripts/docker_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker stop attendance-management-$ATTENDANCE_MANAGEMENT_ENV
-docker rm  -f attendance-management-$ATTENDANCE_MANAGEMENT_ENV
+docker stop attendance-management-uat
+docker rm  -f attendance-management-uat
 
