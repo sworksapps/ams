da5ad462b9ec714443c16c303ad8fc079bfb309e
Piush <piush.mishra@sworks.co.in>
2022-12-02 10:54:02 +0530

    format cronController file

--- controller/cronController.js
+++ controller/cronController.js
@@ -6,27 +6,27 @@ exports.autoCheckoutAtMidnight = async (req, res) => {
   try {
     console.log('Auto Checkout At Midnight Cron task Executed');
     const adminConn = getAdminConnection();
-    if (adminConn){
-      return {status: false, msg: 'The provided admin is not Available'};
+    if (adminConn) {
+      return { status: false, msg: 'The provided admin is not Available' };
     }
     let successFlag = false;
     const ClientTable = await adminConn.model('client_master_datas');
     const tenants = await ClientTable.find({ clientStatus: 1 }).select({ clientDbName: 1 });
-    
+
     //const todayDate = moment().format('YYYY-MM-DD');
     const previousDate = moment().subtract(2, 'days').format('YYYY-MM-DD');
 
     for (const tenant of tenants) {
       const tenantDb = getConnectionByTenant(tenant.clientDbName);
-      if(tenantDb){
+      if (tenantDb) {
         successFlag = true;
         const attModel = await tenantDb.model('attendences_data');
 
         const update = {
           $set: { attendenceStatus: 'AUTOCHECKOUT', primaryStatus: 'ABSENT' },
-          $push: {userStatus: 'SP'}
+          $push: { userStatus: 'SP' }
         };
-        await attModel.updateMany( { date: previousDate, attendenceStatus: 'CLOCKIN' }, update );
+        await attModel.updateMany({ date: previousDate, attendenceStatus: 'CLOCKIN' }, update);
       }
     }
     return { status: successFlag, msg: 'Cron task done' };
