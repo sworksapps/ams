6c9e9f7a4dbaa35d275029a70558cf26ee70a36a
System Administrator <root@ip-10-12-25-53.ap-southeast-1.compute.internal>
2023-01-10 12:25:41 +0530

    SWFR-152 <message>

--- controller/attendenceMobileController.js
+++ controller/attendenceMobileController.js
@@ -118,6 +118,13 @@ exports.checkIn = async (req, res) => {
       });
 
     const userDetails = validateFaceData.data;
+
+    if(userDetails.is_active == 0)
+      return res.status(200).json({
+        statusText: 'FAIL',
+        statusValue: 400,
+        message: 'It seams you are not authorised to checkin, Please contact to your company Admin.',
+      });
   
     if(decodedjwt.clientId == '2137' && userDetails.emp_code == '')
       return res.status(200).json({
@@ -249,6 +256,13 @@ exports.checkOut = async (req, res) => {
       });
   
     const userDetails = validateFaceData.data;
+
+    if(userDetails.is_active == 0)
+      return res.status(200).json({
+        statusText: 'FAIL',
+        statusValue: 400,
+        message: 'It seams you are not authorised to checkout, Please contact to your company Admin.',
+      });
 
     if(decodedjwt.clientId == '2137' && userDetails.emp_code == '')
       return res.status(200).json({
@@ -383,6 +397,13 @@ exports.checkInSubmit = async (req, res) => {
       });
   
     const userDetails = userData.data.data.result[0];
+
+    if(userDetails.is_active == 0)
+      return res.status(200).json({
+        statusText: 'FAIL',
+        statusValue: 400,
+        message: 'It seams you are not authorised to checkin, Please contact to your company Admin.',
+      });
   
     if(userDetails.company_id != decodedjwt.clientId)
       return res.status(200).json({
@@ -511,6 +532,13 @@ exports.checkOutSubmit = async (req, res) => {
       });
   
     const userDetails = userData.data.data.result[0];
+
+    if(userDetails.is_active == 0)
+      return res.status(200).json({
+        statusText: 'FAIL',
+        statusValue: 400,
+        message: 'It seams you are not authorised to checkout, Please contact to your company Admin.',
+      });
   
     if(userDetails.company_id != decodedjwt.clientId)
       return res.status(200).json({
